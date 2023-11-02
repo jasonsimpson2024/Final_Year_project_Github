f3560b1de48c8627ff598a0f8aa672adb3508d7d
u210157 <jason.simpson.2021@mumail.ie>
2023-11-02 12:09:52 +0000

    CI/CD to github attempt 2/11/23

--- src/Components/Cars/Automotive.js
+++ src/Components/Cars/Automotive.js
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../firebase.js';
-import { collection, getDocs, query, limit, startAfter } from 'firebase/firestore';
+import { collection, getDocs, query, orderBy, limit, startAfter } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 
 function Automotive() {
     const [carData, setCarData] = useState([]);
     const [page, setPage] = useState(1);
     const carsPerPage = 5;
-    const [lastDocumentTimestamp, setLastDocumentTimestamp] = useState(null);
+    const [lastDocumentName, setLastDocumentName] = useState(null);
 
     useEffect(() => {
         const fetchData = async (pageNumber) => {
             try {
-                const carsCollection = collection(db, 'Automotive'); // Change the collection name to 'Automotive'
-                let q = query(carsCollection, limit(carsPerPage));
+                const carsCollection = collection(db, 'Automotive');
+                let q = query(carsCollection, orderBy('Name'), limit(carsPerPage));
 
-                if (pageNumber > 1 && lastDocumentTimestamp) {
-                    q = query(carsCollection, startAfter(lastDocumentTimestamp), limit(carsPerPage));
+                if (pageNumber > 1 && lastDocumentName) {
+                    q = query(carsCollection, orderBy('Name'), startAfter(lastDocumentName), limit(carsPerPage));
                 }
 
                 const snapshot = await getDocs(q);
 
                 const newCarData = snapshot.docs.map((doc) => ({
                     id: doc.id,
-                    Name: doc.data().Name, // Display the 'Name' field
-                    Street: doc.data().Street, // Display the 'Location' field
+                    Name: doc.data().Name,
+                    Street: doc.data().Street,
                     Town: doc.data().Town,
                     County: doc.data().County,
                     Eircode: doc.data().Eircode,
@@ -32,7 +32,7 @@ function Automotive() {
 
                 if (newCarData.length > 0) {
                     const lastCar = newCarData[newCarData.length - 1];
-                    setLastDocumentTimestamp(lastCar.selectedSlot);
+                    setLastDocumentName(lastCar.Name);
                 }
 
                 setCarData(newCarData);
@@ -43,7 +43,9 @@ function Automotive() {
         };
 
         fetchData(page);
-    }, [page]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [page]); // Disable the rule for 'lastDocumentName'
+
 
     const nextPage = () => {
         setPage(page + 1);
@@ -52,7 +54,7 @@ function Automotive() {
     const prevPage = () => {
         if (page > 1) {
             if (page === 2) {
-                setLastDocumentTimestamp(null);
+                setLastDocumentName(null);
             }
             setPage(page - 1);
         }
@@ -66,7 +68,6 @@ function Automotive() {
                 <div className="car-details-list">
                     {carData.map((car) => (
                         <div key={car.id} className="car-details">
-                            {/* Use Link component to redirect to BookingCars */}
                             <Link to={`/bookingcars/${car.id}`} className="car-detail">
                                 Name: {car.Name} <br />
                                 Location: {car.Street} {car.Town} {car.County} {car.Eircode}
--- src/Components/HairSalons/hairsalon.js
+++ src/Components/HairSalons/hairsalon.js
@@ -1,49 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../../firebase.js';
-import { collection, getDocs, query, limit, startAfter } from 'firebase/firestore';
+import { collection, getDocs, query, orderBy, limit, startAfter } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 
-function Automotive() {
+function HairSalon() {
     const [carData, setCarData] = useState([]);
     const [page, setPage] = useState(1);
     const carsPerPage = 5;
-    const [lastDocumentTimestamp, setLastDocumentTimestamp] = useState(null);
+    const [lastDocumentName, setLastDocumentName] = useState(null);
 
     useEffect(() => {
         const fetchData = async (pageNumber) => {
             try {
-                const carsCollection = collection(db, 'HairSalon'); // Change the collection name to 'Automotive'
-                let q = query(carsCollection, limit(carsPerPage));
+                const carsCollection = collection(db, 'HairSalon');
+                let q = query(carsCollection, orderBy('Name'), limit(carsPerPage));
 
-                if (pageNumber > 1 && lastDocumentTimestamp) {
-                    q = query(carsCollection, startAfter(lastDocumentTimestamp), limit(carsPerPage));
+                if (pageNumber > 1 && lastDocumentName) {
+                    q = query(carsCollection, orderBy('Name'), startAfter(lastDocumentName), limit(carsPerPage));
                 }
 
                 const snapshot = await getDocs(q);
 
                 const newCarData = snapshot.docs.map((doc) => ({
                     id: doc.id,
-                    Name: doc.data().Name, // Display the 'Name' field
-                    Street: doc.data().Street, // Display the 'Location' field
+                    Name: doc.data().Name,
+                    Street: doc.data().Street,
                     Town: doc.data().Town,
                     County: doc.data().County,
-                    Eircode: doc.data().Eircode, // Display the 'Location' field
+                    Eircode: doc.data().Eircode,
                 }));
 
                 if (newCarData.length > 0) {
                     const lastCar = newCarData[newCarData.length - 1];
-                    setLastDocumentTimestamp(lastCar.selectedSlot);
+                    setLastDocumentName(lastCar.Name);
                 }
 
                 setCarData(newCarData);
-                console.log("Car data updated:", newCarData);
+                console.log("data updated:", newCarData);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
 
         fetchData(page);
-    }, [page]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [page]); // Disable the rule for 'lastDocumentName'
+
 
     const nextPage = () => {
         setPage(page + 1);
@@ -52,7 +54,7 @@ function Automotive() {
     const prevPage = () => {
         if (page > 1) {
             if (page === 2) {
-                setLastDocumentTimestamp(null);
+                setLastDocumentName(null);
             }
             setPage(page - 1);
         }
@@ -66,8 +68,7 @@ function Automotive() {
                 <div className="car-details-list">
                     {carData.map((hair) => (
                         <div key={hair.id} className="car-details">
-                            {/* Use Link component to redirect to BookingCars */}
-                            <Link to={`/bookhairsalon/${hair.id}`} className="car-detail">
+                            <Link to={`/bookingcars/${hair.id}`} className="car-detail">
                                 Name: {hair.Name} <br />
                                 Location: {hair.Street} {hair.Town} {hair.County} {hair.Eircode}
                             </Link>
@@ -83,4 +84,4 @@ function Automotive() {
     );
 }
 
-export default Automotive;
+export default HairSalon;

3760a428a87dc8bc18549302a8ed65db25da3510
u210157 <jason.simpson.2021@mumail.ie>
2023-12-20 16:11:17 +0000

    20/12/23 2

--- src/Components/Barbers/barber.js
+++ src/Components/Barbers/barber.js
@@ -12,7 +12,6 @@ function Barber() {
     const [hasMoreBarbers, setHasMoreBarbers] = useState(true);
     const [selectedCounty, setSelectedCounty] = useState('');
 
-    // List of counties in Ireland
     const counties = ['Antrim', 'Armagh', 'Carlow', 'Cavan', 'Clare', 'Cork', 'Derry', 'Donegal', 'Down', 'Dublin', 'Fermanagh', 'Galway', 'Kerry', 'Kildare', 'Kilkenny', 'Laois', 'Leitrim', 'Limerick', 'Longford', 'Louth', 'Mayo', 'Meath', 'Monaghan', 'Offaly', 'Roscommon', 'Sligo', 'Tipperary', 'Tyrone', 'Waterford', 'Westmeath', 'Wexford', 'Wicklow'];
 
     useEffect(() => {
